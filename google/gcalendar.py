05461a443c16b3e9bfff9c34f28b511f98241eb5
Jeffrey B. Daube <daubejb@gmail.com>
2017-07-24 10:21:29 -0400

    markdown option successfully outputs baseline meeting notes to directory

--- google/gcalendar.py
+++ google/gcalendar.py
@@ -21,20 +21,42 @@ class Meeting:
     '''Represents a meeting on the google calendar.'''
     def __init__(self, meeting):
         '''Initialize a meeting with summary, dates, times, and creator.'''
+
         self.meeting = {
             'id': meeting['id'],
             'summary': meeting['summary'],
             'start_date': meeting['start'].get('dateTime')[:10],
-            'start_time': meeting['start'].get('dateTime')[11:16],
             'end_date': meeting['end'].get('dateTime')[:10],
-            'end_time': meeting['end'].get('dateTime')[11:16],
-            'location': meeting['location'],
-            'description': meeting['description'],
             'organizer': meeting['organizer'].get('email'),
             'html_link': meeting['htmlLink'],
-            'revisions': meeting['sequence'],
-            'attendees': meeting['attendees'],
         }
+        try:
+            self.meeting['start_time'] = meeting['start'].get(
+                'dateTime')[11:16]
+        except TypeError:
+            self.meeting['start_time'] = 'all day'
+
+        try:
+            self.meeting['end_time'] = meeting['end'].get(
+                'dateTime')[11:16]
+        except TypeError:
+            self.meeting['end_time'] = 'all day'
+
+        try:
+            self.meeting['description'] = meeting['description']
+        except KeyError:
+            self.meeting['description'] = 'No description provided.'
+
+        try:
+            self.meeting['location'] = meeting['location']
+        except KeyError:
+            self.meeting['location'] = 'No location provided.'
+
+        try:
+            self.meeting['attendees'] = meeting['attendees']
+        except KeyError:
+            self.meeting['attendees'] = 'No attendees'
+
         return
         # self.start_date = start_date
         # self.end_date = end_date
--- main.py
+++ main.py
@@ -29,49 +29,53 @@ def main():
     gcalendar = gCalendar()
     gcalendar.new_meeting(meetings)
 
-    for key, value in gcalendar.meetings[0].meeting:
-        print('{}: {}'.format(key, value))
+    if args.markdown:
+        for meeting in gcalendar.meetings:
+            markdown_filename = '{} - {}.md'.format(
+                meeting.meeting['start_date'],
+                meeting.meeting['summary'])
+            hr = '_ _ _\n\n'
+            chars_to_remove = dict((ord(char), None) for char in '\\/*?:"<>|')
+            clean_md_filename = markdown_filename.translate(chars_to_remove)
+            f = open(clean_md_filename, 'w')
+            f.write('# {}\n\n'.format(meeting.meeting['summary']))
+            f.write('**Organizer**: {}\n\n'.format(
+                meeting.meeting['organizer']
+            ))
+            f.write('**Start**: {} - {}  | \
+                    **End**: {} - {}  | \
+                    **Location**: {}\n\n'.format(
+                meeting.meeting['start_date'],
+                meeting.meeting['start_time'],
+                meeting.meeting['end_date'],
+                meeting.meeting['end_time'],
+                meeting.meeting['location']
+            ))
+            f.write(hr)
+            f.write('## Notes:\n\n')
+            f.write('1. take notes here\n\n')
+            f.write(hr)
+            f.write('## Description:\n\n{}\n\n'.format(
+                meeting.meeting['description']
+            ))
+            f.write(hr)
+            f.write('**Calendar link**: {}\n\n'.format(
+                meeting.meeting['html_link']
+            ))
+            f.write('**Attendees**:\n\n')
+            attendee_builder = ''
+            for attendee in meeting.meeting['attendees']:
+                email = attendee.get('email')
+                response_status = attendee.get('responseStatus')
+                if 'resource' not in email:
+                    attendee_builder = attendee_builder + '{}: {}\n\n'.format(
+                        email, response_status
+                    )
+            f.write(attendee_builder)
+            f.close()
 
-
-
-    # for meeting in meetings:
-    #     start_date_time = meeting['start'].get('dateTime')
-    #     end_date_time = meeting['end'].get('dateTime')
-    #     summary = meeting['summary']
-    #     try:
-    #         date, start_time = parse_date_time(start_date_time)
-    #         date_not_used, end_time = parse_date_time(end_date_time)
-    #     except TypeError:
-    #         start_time = 'all day'
-    #         end_time = 'all day'
-    #     try:
-    #         description = meeting['description']
-    #     except KeyError:
-    #         description = 'No description provided'
-    #     try:
-    #         location = meeting['location']
-    #     except KeyError:
-    #         location = 'No location provided'
-    #     creator = meeting['creator'].get('email')
-    #     htmlLink = meeting['htmlLink']
-    #     try:
-    #         attendees = meeting['attendees']
-    #     except KeyError:
-    #         attendees = 'No attendees'
-    #     markdown_filename = date + '-' + summary + '.md'
-
-    #     if args.markdown:
-    #         pass
-    #         # fsummary = '# {}\n\n'.format(summary)
-    #         # fdate = '**Date**: {}  |  '.format(date)
-    #         # fstart_time = '**Start time**: {}  |  '.format(start_time)
-    #         # fend_time = '**End time**:   {}  |  '.format(end_time)
-    #         # flocation = '**Location**:   {}\n\n'.format(location)
-    #         # fhr = '_ _ _\n\n'
-    #         # fnotes = '## Notes:\n\n'
-    #         # fnote1 = '1. note\n\n'
-    #         # fdescription = '## Description:\n\n {}\n\n'.format(description)
-    #         # fcreator = '**Creator**: {}\n\n'.format(creator)
+    if args.google:
+        print('google it is')
     #         # flink = 'Calendar link: {}\n\n'.format(htmlLink)
     #         # fattendees_header = '**Attendees**:\n\n'
     #         # fperson = ''
@@ -84,26 +88,7 @@ def main():
     #         #         if 'resource' not in email:
     #         #             fperson = fperson + '{}: {}\n\n'.format(
     #         #                 email, response_status)
-    #         # f = open(markdown_filename, 'w')
-    #         # f.write(fsummary)
-    #         # f.write(fcreator)
-    #         # f.write(fdate)
-    #         # f.write(fstart_time)
-    #         # f.write(fend_time)
-    #         # f.write(flocation)
-    #         # f.write(fhr)
-    #         # f.write(fnotes)
-    #         # f.write(fnote1)
-    #         # f.write(fhr)
-    #         # f.write(fdescription)
-    #         # f.write(fhr)
-    #         # f.write(fattendees_header)
-    #         # f.write(fperson)
-    #         # f.write(flink)
-    #         # f.close()
 
-    #     if args.google:
-    #         print('google it is')
 
 if __name__ == '__main__':
     main()
