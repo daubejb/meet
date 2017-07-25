f08bc574bc52616f1e03aeeaf8ab013a663d1dd6
Jeffrey B. Daube <daubejb@gmail.com>
2017-07-25 14:23:36 -0400

    revert version to 1.0 prod pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='meet',
-    version='1.1',
+    version='1.0',
     author='Jeffrey B. Daube',
     author_email='daubejb@gmail.com',
     packages=find_packages(),
