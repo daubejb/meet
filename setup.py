92b37c8ef2546d90d10e0b722e1f1e9d5dab5c08
Jeffrey B. Daube <daubejb@gmail.com>
2017-07-25 14:10:17 -0400

    change version to 1.1 in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='meet',
-    version='1.0',
+    version='1.1',
     author='Jeffrey B. Daube',
     author_email='daubejb@gmail.com',
     packages=find_packages(),
