6389bd2ec258425174480168557ea950a99badaf
Jeffrey B. Daube <daubejb@gmail.com>
2017-07-25 22:36:24 -0400

    made -g and -m mutually exclusive in argparse.  cannot create a markdown document and a google document at the same time

--- meet/terminal/terminal.py
+++ meet/terminal/terminal.py
@@ -13,19 +13,20 @@ def get_user_input():
         parser = argparse.ArgumentParser(description='a cli application to \
                 create a meeting notes google doc or local markdown file \
                 current or impending meeting on your google calendar')
-        parser.add_argument('-g', '--google',
-                            help='create a new google doc with minutes',
-                            action='store_true',
-                            dest='google')
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument('-g', '--google',
+                           help='create a new google doc with minutes',
+                           action='store_true',
+                           dest='google')
+        group.add_argument('-m', '--markdown',
+                           help='create a local markdown file with minutes',
+                           action='store_true',
+                           dest='markdown')
         parser.add_argument('-s', '--share',
                             help='automatically share the google doc with \
                                 meeting participants',
                             action='store_true',
                             dest='share')
-        parser.add_argument('-m', '--markdown',
-                            help='create a local markdown file with minutes',
-                            action='store_true',
-                            dest='markdown')
         args = parser.parse_args()
         return args
     except ImportError:
