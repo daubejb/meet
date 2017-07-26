0fdcf90abade97f87ba74f28eeb7c75930da1291
Jeffrey B. Daube <daubejb@gmail.com>
2017-07-26 15:47:33 -0400

    v1.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='meet',
-    version='1.3.1',
+    version='1.3.2',
     author='Jeffrey B. Daube',
     author_email='daubejb@gmail.com',
     packages=find_packages(),
